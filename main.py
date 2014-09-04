4745e42bf7f7c4d7498bca21c787000d65e6fa46
bigfoot <anon.nerdword@gmail.com>
2014-09-04 00:37:14 -0700

    init commit

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
 __author__ = 'bigfoot'
 
-import os
 import time
 import socket
 
-for i in range(1,256):
+
+hostname = raw_input('Enter Host to scan: ')
+
+for i in range(1, 256):
+
     s = socket.socket()
+    s.settimeout(.05)
+    ip = socket.gethostbyname(hostname)
+
+    response = s.connect_ex((ip,i))
+
+    if response:
+        print ("%d\tclose" %i)
+    else:
+        print ("%d\topen" %i)
+
+    s.close()
